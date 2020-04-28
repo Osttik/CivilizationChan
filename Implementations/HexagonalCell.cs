e62e4ecca338b5e35057da00d9bc26d821f8e6f3
Adrian-Ostap Manchur <aom@interlogic.com.ua>
2020-04-28 18:00:00 +0300

    Added

---
+++ Implementations/HexagonalCell.cs
@@ -0,0 +1,19 @@
+﻿using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Implementations
+{
+    public class HexagonalCell : ICell
+    {
+        public string Id { get; set; }
+        
+        public List<HexagonalCell> Neighborhood { get; set; }
+
+        public HexagonalCell()
+        {
+            Neighborhood = new List<HexagonalCell>(6);
+        }
+    }
+}
---
+++ Implementations/Map.cs
@@ -0,0 +1,17 @@
+﻿using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Implementations
+{
+    public class Map
+    {
+        public List<ICell> Cells { get; set; }
+
+        public ICell GetCellById(string id)
+        {
+            return Cells.Find(x => x.Id == id);
+        }
+    }
+}
